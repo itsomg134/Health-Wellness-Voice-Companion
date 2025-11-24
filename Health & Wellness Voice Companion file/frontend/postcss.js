0cfb481edf3429591ba5b031801a554ef54fec1e
itsomg134 <omgedam123098@gmail.com>
2025-11-24 17:26:09 +0530

    Add files via upload

---
+++ Health & Wellness Voice Companion file/frontend/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./build/chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("build/chunks/[turbopack-node]_transforms_postcss_ts_b22141b9._.js")
+R.c("build/chunks/[root-of-the-server]__4daba124._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/OneDrive/Desktop/DAILY WELLNESS VOICE COMPANION/frontend/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/OneDrive/Desktop/DAILY WELLNESS VOICE COMPANION/frontend/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
